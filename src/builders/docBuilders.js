26d2bc7474d14a8d2546c7830ecdf28f996a8e29
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-03-24 00:22:15 +0200

    added method to nodeDocBuilder

--- src/builders/docBuilders.js
+++ src/builders/docBuilders.js
@@ -3,22 +3,47 @@ module.exports = {
     buildDocs(stats) {
         let documents = [];
         let nodesStats = stats.nodeStats.body.nodes;
+        if(!nodesStats){
+            return this.getEmptyNodeObject()
+        }
         let nodeDocs = this.buildNodeDocs(nodesStats);
+        console.log(nodeDocs);
         process.exit();
     },
     buildNodeDocs(nodesStats){
         let nodesDocs = [];
         let nodeArrKeys = Object.keys(nodesStats);
-        for(let i=0; i <= nodeArrKeys.length; i++){
+        for(let i=0; i < nodeArrKeys.length; i++){
             let nodeKey = nodeArrKeys[i];
-            let nodeDocument = {
+            nodesDocs.push({
                 uuid: nodeArrKeys[i],
-                cluster_name: this.getNodeCluster(nodesStats[nodeKey])
-            }
+                cluster_name: this.getNodeCluster(nodesStats[nodeKey]),
+                ip: this.getNodeIP(nodesStats[nodeKey]),
+                avg_query_time: this.getNodeAvgQueryTime(nodesStats[nodeKey])
+            })
         }
+        return nodesDocs;
     },
     getNodeCluster(nodeStats){
-        console.log(nodeStats);
-        process.exit(0);
+        return nodeStats.name;
+    },
+    getNodeIP(nodeStats){
+        return nodeStats.ip;
+    },
+    getNodeAvgQueryTime(nodeStats, nodeToken){
+        if(!this.prevStats || nodeStats){
+            return null;
+        }
+        let currentQueyTime = nodeStats.indices.search.query_time_in_millis;
+        let oldQueyTime = prevStats[nodeToken].indices.search.query_time_in_millis;
+        return currentQueyTime - oldQueyTime;    
+    },
+    getEmptyNodeObject() {
+        return [{
+            uuid: null,
+            cluster_name: null,
+            ip: null,
+            avg: 0
+        }]
     }
 }
--- src/utils/fetcherUtils.js
+++ src/utils/fetcherUtils.js
@@ -2,13 +2,6 @@ module.exports = {
     getISOTimestamp() {
         return new Date().toISOString();
     },
-    getClusterInfo(currentStats) {
-        let resp = currentStats.body;
-        let clusterInfo = {};
-        clusterInfo['cluster_name'] = resp.cluster_name;
-        clusterInfo['sum_of_nodes'] = resp._nodes.successful;
-        return clusterInfo;
-    },
     sendToFile(){
         fs.writeFileSync('nodeStats.json', JSON.stringify(stats.nodeStats, null, 2))
         fs.writeFileSync('indexStats.json', JSON.stringify(stats.indexStats, null, 2))
